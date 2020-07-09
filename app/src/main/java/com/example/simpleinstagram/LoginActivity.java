fc68d866cfc18dc4b519b5e6ba0d2b11e0bed101
Enrique B Montas <enriquem@fb.com>
2020-07-09 17:35:09 -0400

    signup added

--- app/src/main/java/com/example/simpleinstagram/LoginActivity.java
+++ app/src/main/java/com/example/simpleinstagram/LoginActivity.java
@@ -7,14 +7,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.example.simpleinstagram.databinding.ActivityLoginBinding;
 import com.parse.LogInCallback;
+import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -22,6 +26,8 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Switch swSignup;
+    private Button btnSignup;
     private ImageView ivIGLogo;
 
     @Override
@@ -40,6 +46,22 @@ public class LoginActivity extends AppCompatActivity {
         etPassword = binding.etPassword;
         btnLogin = binding.btnLogin;
         ivIGLogo = binding.ivIGLogo;
+        swSignup = binding.swSignup;
+        btnSignup = binding.btnSignup;
+
+        swSignup.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    btnLogin.setVisibility(View.GONE);
+                    btnSignup.setVisibility(View.VISIBLE);
+                } else {
+                    btnLogin.setVisibility(View.VISIBLE);
+                    btnSignup.setVisibility(View.GONE);
+                }
+            }
+        });
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,7 +72,34 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick signup button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                signupUser(username, password);
+            }
+        });
 
+    }
+
+    private void signupUser(String username, String password) {
+        Log.i(TAG, "signing up user: " + username);
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Issue with Signup", e);
+                    return;
+                }
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Welcome", Toast.LENGTH_SHORT);
+            }
+        });
     }
 
     private void loginUser(String username, String password) {
@@ -59,7 +108,6 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void done(ParseUser user, ParseException e) {
                 if (e != null){
-                    // TODO: better error handling
                     Log.e(TAG, "Issue with login", e);
                     return;
                 }
